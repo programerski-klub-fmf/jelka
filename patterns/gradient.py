bc7af530df8c614df78f212477ca94f9926f2de3
bor <bor.brudar@gmail.com>
2023-12-10 00:21:49 +0100

    changed gradient so it's dynamic

--- patterns/gradient.py
+++ patterns/gradient.py
@@ -6,7 +6,5 @@ jelka = Jelka(file="data/random_tree.csv")
 
 @jelka.run_shader
 def color_change(id: int, time: int) -> Color:
-    #offset = math.fabs(math.sin(time))*255 #max +-1
-    offset = 0
     pos = list(jelka.get_pos(id))
-    return (pos[0] * 255,pos[1] * 255,pos[2] * 255)
+    return ((pos[0] * 255 + math.sin(time/5000+1)*255 + 256) %256,(pos[1] * 255 + math.sin(time/5000 + 2)*255 + 256) %256,(pos[2] * 255 + math.sin(time/5000)*255 + 256) %256)
