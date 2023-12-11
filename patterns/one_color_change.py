1e91d5ad7ed82f0fa72e8c988b933702e718da5a
Filip Š <filip.stamcar@hotmail.com>
2023-12-11 23:47:25 +0100

    Update one color change

--- patterns/one_color_change.py
+++ patterns/one_color_change.py
@@ -1,12 +1,16 @@
-from library.jelka import Jelka, Color, Id, Position, Time
+from colorsys import hsv_to_rgb, rgb_to_hsv
+
+from library.jelka import Color, Jelka
 
 jelka = Jelka(file="data/lucke3d.csv")
 
 
 @jelka.run_shader
 def color_change(id: int, time: int) -> Color:
-    # TODO: Kaj sploh dela ta vzorec ker ne zgleda prav?
-    # color = jelka.get_color(id)
-    x, y, z = jelka.get_pos(id)
-    z *= 256
-    return (z % 256, z % 256, z % 256)
+    hue = rgb_to_hsv(*jelka.get_color(id))[0] * 360
+    hue = (hue + 1) % 360
+
+    color = hsv_to_rgb(hue / 360.0, 1.0, 1.0)
+    color = tuple(map(lambda x: int(x * 255), color))
+
+    return Color(color)
