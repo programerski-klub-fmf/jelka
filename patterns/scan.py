894cc12af78fd23003cb19f943fbbc134d47c976
Filip Š <filip.stamcar@hotmail.com>
2023-12-12 11:48:22 +0100

    Make scan better

--- patterns/scan.py
+++ patterns/scan.py
@@ -1,29 +1,34 @@
-import random as r
-from library.jelka import Jelka, Color, Id, Position, Time
-from typing import Any, Callable, cast
 import math
-from library.patterns_lib import vivid, random_color
+
+from library.jelka import Color, Jelka
+from library.patterns_lib import random_color, vivid
 
 jelka = Jelka(file="data/lucke3d.csv")
 
+axis = 2
+threshold = 0.1
+
 
 @jelka.run_shader_all
-def update_colors(colors: list(Color), time: int, frame: int):
-    zup = math.sin(frame / 80)/1.8 + 0.55
-    threshold = 0.1
+def update_colors(colors: list[Color], time: int, frame: int):
+    coord = math.sin(frame / 80) / 1.8 + 0.55
 
-    global col
+    global color
     global change
-    if frame == 0: change = 0
-    if (frame == 0 or zup < 0 or zup > 1.1) and change == 0:
+
+    if frame == 0:
+        change = 0
+
+    if (frame == 0 or coord < 0 or coord > 1.1) and change == 0:
+        color = vivid(random_color())
         change = 1
-        col = vivid(random_color())
-    elif zup > 0.01 and zup < 0.99: change = 0
+    elif 0.01 < coord < 0.99:
+        change = 0
 
     for i in range(0, len(colors)):
         pos = jelka.get_pos(i)
-        if pos[2] <= zup and pos[2] >= zup - threshold:
-            colors[i] = col
+        if coord >= pos[axis] >= coord - threshold:
+            colors[i] = color
         else:
             colors[i] = (0, 0, 0)
 
