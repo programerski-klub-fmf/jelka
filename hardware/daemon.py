645c8ef0a9c79bb12e5c39bce2c09862dcb7ce2a
Filip Š <filip.stamcar@hotmail.com>
2023-12-16 22:17:35 +0100

    Improve daemon a bit

--- hardware/daemon.py
+++ hardware/daemon.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
-import flask
-import threading
-import os
-import secrets
-import subprocess
-import hashlib
 import base64
-import time
+import hashlib
+import os
+import re
+import secrets
 import signal
+import subprocess
+import threading
+import time
+
+import flask
 
 http = flask.Flask(__name__)
-onemogočeni = set(["onemogočen.py"])
-skrivnost = secrets.token_bytes(8)
-čas_vzorca = 30
-proces = None
-naslednji = None
+secret = secrets.token_bytes(8)
 
-def trenuten():
-	if proces:
-		return proces.args[1]
+name_regex = re.compile(r"# *NAME: ((?:[-_+*.!? ]|\w)+)")
+
+pattern_time = int(os.getenv("PATTERN_TIME", 30))
+
+disabled_patterns = set(os.getenv("DISABLED_PATTERNS", "").split(","))
+hidden_patterns = set(os.getenv("HIDDEN_PATTERNS", "").split(","))
+
+
+next_pattern = None
+
+process = None
+
+def current_pattern():
+	if process:
+		return process.args[1]
 	return None
 
 def json_status():
-	return "data: " + http.json.dumps({"čas": čas_vzorca, "vzorci": os.listdir("../patterns"), "trenuten": trenuten(), "onemogočeni": [x for x in onemogočeni]}) + "\n\n"
+	return "data: " + http.json.dumps({"čas": pattern_time, "vzorci": os.listdir("../patterns"), "trenuten": current_pattern(), "onemogočeni": [x for x in disabled_patterns]}) + "\n\n"
 
 def http_event_stream():
 	yield json_status()
@@ -47,23 +57,23 @@ def stream():
 
 @http.route("/<koda>", methods=["POST"])
 def update(koda):
-	global onemogočeni
-	global čas_vzorca
-	global naslednji
-	if koda not in kode():
+	global disabled_patterns
+	global pattern_time
+	global next_pattern
+	if not koda or koda not in kode():
 		return flask.Response(http.json.dumps({"napaka": {"koda": 1, "besedilo": "napačna koda"}}), status=200)
 	data = flask.json.loads(flask.request.data)
 	if "omogoči" in data.keys():
-		onemogočeni -= set(data["omogoči"])
+		disabled_patterns -= set(data["omogoči"])
 	if "onemogoči" in data.keys():
-		onemogočeni |= set(data["onemogoči"])
+		disabled_patterns |= set(data["onemogoči"])
 	if "čas" in data.keys():
 		if data["čas"] < 1 or data["čas"] > 300:
 			return flask.Response(http.json.dumps({"napaka": {"koda": 2, "besedilo": "čas ni v [1,300]"}}), status=200)
-		čas_vzorca = data["čas"]
+		pattern_time = data["čas"]
 	sprememba_stanja.set()
 	if "začni" in data.keys() and type(data["začni"]) == str:
-		naslednji = data["začni"]
+		next_pattern = data["začni"]
 		začni_vzorec.set()
 	return "true"
 
@@ -72,37 +82,40 @@ def update_preflight(koda):
 	return flask.Response("", status=204, headers={"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET, POST, OPTIONS", "Access-Control-Allow-Headers": "Content-Type", "Access-Control-Max-Age": "86400"})
 
 def kode():
-	return [base64.b64encode(hashlib.sha256(skrivnost+bytes(str(int(time.time()/1000)), encoding="utf-8")).digest()).replace(b"/", b"").replace(b"+", b"")[:4].lower().decode(), base64.b64encode(hashlib.sha256(skrivnost+bytes(str(int(time.time()/1000)-1), encoding="utf-8")).digest()).replace(b"/", b"").replace(b"+", b"")[:4].lower().decode(), os.getenv("KODA")]
+	return [base64.b64encode(hashlib.sha256(secret + bytes(str(int(time.time() / 1000)), encoding="utf-8")).digest()).replace(b"/", b"").replace(b"+", b"")[:6].lower().decode(), base64.b64encode(hashlib.sha256(secret + bytes(str(int(time.time() / 1000) - 1), encoding="utf-8")).digest()).replace(b"/", b"").replace(b"+", b"")[:4].lower().decode(), os.getenv("KODA")]
 
 def zaznaj_smrt():
-	proces.wait()
+	process.wait()
 	začni_vzorec.set()
 
 die = False
 subprocess_manager_thread = None
 
 def subprocess_manager():
-	global naslednji
-	global proces
+	global next_pattern
+	global process
 	global začni_vzorec
 	while True:
-		if not naslednji:
-			vzorci = set(os.listdir("../patterns"))-set(["začasen.py"])-onemogočeni
-			naslednji = sorted(vzorci)[0]
+		if len(vzorci) == 0:
+			continue
+		if not next_pattern:
+			vzorci = set(os.listdir("../patterns")) - set(["začasen.py"]) - disabled_patterns
+			next_pattern = sorted(vzorci)[0]
 		if die:
 			return
-		print("daemon: starting " + naslednji)
-		proces = subprocess.Popen(["./wrapper.py", naslednji])
+		print("daemon: starting " + next_pattern)
+		process = subprocess.Popen(["python", "./wrapper.py", next_pattern])
 		sprememba_stanja.set()
-		vzorci = set(os.listdir("../patterns"))-set(["začasen.py"])-onemogočeni
-		naslednji = sorted(vzorci)[0]
-		if trenuten() in vzorci:
-			naslednji = sorted(vzorci)[(sorted(vzorci).index(trenuten())+1)%len(vzorci)]
+		vzorci = set(os.listdir("../patterns")) - set(["začasen.py"]) - disabled_patterns
+		next_pattern = sorted(vzorci)[0]
+		if current_pattern() in vzorci:
+			next_pattern = sorted(vzorci)[(sorted(vzorci).index(current_pattern()) + 1) % len(vzorci)]
+		print("TRENUTEN", current_pattern(), "NASLEDNJI", next_pattern, "VSI", vzorci)
 		začni_vzorec.clear()
 		threading.Thread(target=zaznaj_smrt).start()
-		razlog = začni_vzorec.wait(timeout=čas_vzorca)
-		proces.send_signal(subprocess.signal.SIGINT)
-		proces.wait()
+		začni_vzorec.wait(timeout=pattern_time)
+		process.send_signal(subprocess.signal.SIGINT)
+		process.wait()
 
 začni_vzorec = threading.Event()
 sprememba_stanja = threading.Event()
@@ -110,7 +123,7 @@ sprememba_stanja = threading.Event()
 def samomor(signal_descriptor, stack_frame):
 	global die
 	die = True
-	global proces
+	global process
 	global subprocess_manager_thread
 	print("daemon: exiting")
 	if subprocess_manager_thread:
@@ -118,9 +131,9 @@ def samomor(signal_descriptor, stack_frame):
 		subprocess_manager_thread.join()
 	else:
 		print("daemon: subprocess_manager_thread was not started, not waiting to join.")
-	proces.send_signal(subprocess.signal.SIGINT)
+	process.send_signal(subprocess.signal.SIGINT)
 	print("daemon: waiting for proces to terminate")
-	proces.wait()
+	process.wait()
 	os._exit(0)
 
 if __name__ == "__main__":
