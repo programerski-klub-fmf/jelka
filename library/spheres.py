d9f766d302de7a26791b1d784348d0af42324d91
Filip Š <filip.stamcar@hotmail.com>
2023-12-16 23:28:37 +0100

    Reformat library

--- library/spheres.py
+++ library/spheres.py
@@ -3,7 +3,14 @@ from library.types import Position
 
 
 class Sphere:
-    def __init__(self, center: Position, radius: float, startpos : Position =(0.5,0.5,0.5), endpos : Position =(0.5,0.5,0.5),speed : float = 0.1) -> None:
+    def __init__(
+        self,
+        center: Position,
+        radius: float,
+        startpos: Position = (0.5, 0.5, 0.5),
+        endpos: Position = (0.5, 0.5, 0.5),
+        speed: float = 0.1,
+    ) -> None:
         self.center = center
         self.radius = radius
         self.startpospos = startpos
@@ -18,10 +25,10 @@ class Sphere:
             return True
         return False
 
-    def set_center(self,center : Position) -> None:
+    def set_center(self, center: Position) -> None:
         self.center = center
 
-    def set_radius(self,radius : float) -> None:
+    def set_radius(self, radius: float) -> None:
         self.radius = radius
 
     def get_center(self) -> Position:
@@ -35,7 +42,7 @@ class Sphere:
 
     def set_end(self, end: Position) -> None:
         self.endpos = end
-        
+
     def get_start(self) -> Position:
         return self.startpos
 
@@ -44,11 +51,21 @@ class Sphere:
         self.speed = speed
 
     def update_pos(self) -> None:
-        vec = (self.endpos[0] - self.startpos[0], self.endpos[1] - self.startpos[1], self.endpos[2] - self.startpos[2])
+        vec = (
+            self.endpos[0] - self.startpos[0],
+            self.endpos[1] - self.startpos[1],
+            self.endpos[2] - self.startpos[2],
+        )
         self.center = (
             self.center[0] + vec[0] * self.speed,
             self.center[1] + vec[1] * self.speed,
             self.center[2] + vec[2] * self.speed,
         )
-        if dist(self.sphereDiff(self.startpos)) > dist((self.startpos[0]-self.endpos[0],self.startpos[1]-self.endpos[1],self.startpos[2]-self.endpos[2])):
+        if dist(self.sphereDiff(self.startpos)) > dist(
+            (
+                self.startpos[0] - self.endpos[0],
+                self.startpos[1] - self.endpos[1],
+                self.startpos[2] - self.endpos[2],
+            )
+        ):
             self.center = self.startpos
