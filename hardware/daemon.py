1d031bb73e9d01281e81cc8a8fd3db3f595d3dae
Filip Š <filip.stamcar@hotmail.com>
2023-12-17 14:01:23 +0100

    Improve daemon and website

--- hardware/daemon.py
+++ hardware/daemon.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 import base64
 import hashlib
 import os
+import pathlib
 import re
 import secrets
 import signal
@@ -18,27 +20,44 @@ name_regex = re.compile(r"# *NAME: ((?:[-_+*.!? ]|\w)+)")
 
 pattern_time = int(os.getenv("PATTERN_TIME", 30))
 
-disabled_patterns = set(os.getenv("DISABLED_PATTERNS", "").split(","))
-hidden_patterns = set(os.getenv("HIDDEN_PATTERNS", "").split(","))
+disabled_patterns = set(os.getenv("DISABLED_PATTERNS", "").split(",")) - {""}
+hidden_patterns = set(os.getenv("HIDDEN_PATTERNS", "").split(",")) - {""}
+
+forced_pattern: str | None = None
+current_pattern: str | None = None
+current_process: subprocess.Popen | None = None
 
 
-next_pattern = None
+def get_patterns():
+	patterns = set()
 
-process = None
+	for pattern in pathlib.Path("../patterns/").iterdir():
+		if not pattern.is_file():
+			continue
 
-def current_pattern():
-	if process:
-		return process.args[1]
-	return None
+		if pattern.name in hidden_patterns:
+			continue
+
+		name = pattern.name
+
+		for line in pattern.open(encoding="utf-8"):
+			if match := name_regex.search(line):
+				name = match.group(1)
+				break
+
+		# name, filename, enabled
+		patterns.add((name, pattern.name, pattern.name not in disabled_patterns))
+
+	return sorted(patterns)
 
 def json_status():
-	return "data: " + http.json.dumps({"čas": pattern_time, "vzorci": os.listdir("../patterns"), "trenuten": current_pattern(), "onemogočeni": [x for x in disabled_patterns]}) + "\n\n"
+	return "data: " + http.json.dumps({"time": pattern_time, "patterns": get_patterns(), "disabled": list(disabled_patterns), "current": current_pattern}) + "\n\n"
 
 def http_event_stream():
 	yield json_status()
 	while sprememba_stanja.wait():
 		yield json_status()
-		print("geslo je: " + kode()[0])
+		print("daemon: current password: " + kode()[0])
 		sprememba_stanja.clear()
 
 @http.route("/vzorci/<vzorec>", methods=["GET"])
@@ -59,21 +78,22 @@ def stream():
 def update(koda):
 	global disabled_patterns
 	global pattern_time
-	global next_pattern
+	global forced_pattern
 	if not koda or koda not in kode():
-		return flask.Response(http.json.dumps({"napaka": {"koda": 1, "besedilo": "napačna koda"}}), status=200)
+		return flask.Response(http.json.dumps({"napaka": {"koda": 1, "besedilo": "Napačno geslo!"}}), status=200)
 	data = flask.json.loads(flask.request.data)
+	print("daemon: received data:", data)
 	if "omogoči" in data.keys():
 		disabled_patterns -= set(data["omogoči"])
 	if "onemogoči" in data.keys():
 		disabled_patterns |= set(data["onemogoči"])
 	if "čas" in data.keys():
-		if data["čas"] < 1 or data["čas"] > 300:
-			return flask.Response(http.json.dumps({"napaka": {"koda": 2, "besedilo": "čas ni v [1,300]"}}), status=200)
+		if data["čas"] < 1 or data["čas"] > 600:
+			return flask.Response(http.json.dumps({"napaka": {"koda": 2, "besedilo": "Čas mora biti med 1 in 600 sekund!"}}), status=200)
 		pattern_time = data["čas"]
 	sprememba_stanja.set()
 	if "začni" in data.keys() and type(data["začni"]) == str:
-		next_pattern = data["začni"]
+		forced_pattern = data["začni"]
 		začni_vzorec.set()
 	return "true"
 
@@ -85,37 +105,47 @@ def kode():
 	return [base64.b64encode(hashlib.sha256(secret + bytes(str(int(time.time() / 1000)), encoding="utf-8")).digest()).replace(b"/", b"").replace(b"+", b"")[:6].lower().decode(), base64.b64encode(hashlib.sha256(secret + bytes(str(int(time.time() / 1000) - 1), encoding="utf-8")).digest()).replace(b"/", b"").replace(b"+", b"")[:4].lower().decode(), os.getenv("KODA")]
 
 def zaznaj_smrt():
-	process.wait()
+	current_process.wait()
 	začni_vzorec.set()
 
 die = False
 subprocess_manager_thread = None
 
 def subprocess_manager():
-	global next_pattern
-	global process
+	global forced_pattern
+	global current_pattern
+	global current_process
 	global začni_vzorec
+
 	while True:
-		if len(vzorci) == 0:
-			continue
-		if not next_pattern:
-			vzorci = set(os.listdir("../patterns")) - set(["začasen.py"]) - disabled_patterns
-			next_pattern = sorted(vzorci)[0]
 		if die:
 			return
-		print("daemon: starting " + next_pattern)
-		process = subprocess.Popen(["python", "./wrapper.py", next_pattern])
+
+		patterns = [pattern[1] for pattern in get_patterns() if pattern[2]]
+		print(f"daemon: enabled patterns: {list(patterns)}")
+		print(f"daemon: disabled patterns: {list(disabled_patterns)}")
+
+		if not patterns:
+			sprememba_stanja.wait(timeout=5)
+			continue
+
+		try: current_pattern = patterns[(patterns.index(current_pattern) + 1) % len(patterns)]
+		except ValueError: current_pattern = patterns[0]
+
+		if forced_pattern:
+			current_pattern = forced_pattern
+			forced_pattern = None
+
+		print(f"daemon: starting pattern {current_pattern}")
+		current_process = subprocess.Popen(["./wrapper.py", current_pattern])
+
 		sprememba_stanja.set()
-		vzorci = set(os.listdir("../patterns")) - set(["začasen.py"]) - disabled_patterns
-		next_pattern = sorted(vzorci)[0]
-		if current_pattern() in vzorci:
-			next_pattern = sorted(vzorci)[(sorted(vzorci).index(current_pattern()) + 1) % len(vzorci)]
-		print("TRENUTEN", current_pattern(), "NASLEDNJI", next_pattern, "VSI", vzorci)
 		začni_vzorec.clear()
+
 		threading.Thread(target=zaznaj_smrt).start()
 		začni_vzorec.wait(timeout=pattern_time)
-		process.send_signal(subprocess.signal.SIGINT)
-		process.wait()
+		current_process.send_signal(subprocess.signal.SIGINT)
+		current_process.wait()
 
 začni_vzorec = threading.Event()
 sprememba_stanja = threading.Event()
@@ -123,26 +153,29 @@ sprememba_stanja = threading.Event()
 def samomor(signal_descriptor, stack_frame):
 	global die
 	die = True
-	global process
+	global current_process
 	global subprocess_manager_thread
 	print("daemon: exiting")
 	if subprocess_manager_thread:
-		print("daemon: subprocess_manager_thread: waiting to join ...")
+		print("daemon: waiting to join subprocess_manager_thread")
 		subprocess_manager_thread.join()
 	else:
-		print("daemon: subprocess_manager_thread was not started, not waiting to join.")
-	process.send_signal(subprocess.signal.SIGINT)
+		print("daemon: subprocess_manager_thread was not started, not waiting to join")
+	current_process.send_signal(subprocess.signal.SIGINT)
 	print("daemon: waiting for proces to terminate")
-	process.wait()
+	current_process.wait()
 	os._exit(0)
+
 
 if __name__ == "__main__":
 	signal.signal(signal.SIGINT, samomor)
 	signal.signal(signal.SIGHUP, samomor)
 	signal.signal(signal.SIGTERM, samomor)
-	port = 6969
-	if os.getenv("PORT"):
-		port = int(os.getenv("PORT"))
-	threading.Thread(target=lambda: http.run(host="::", port=port, debug=True, use_reloader=False)).start()
+
+	port = int(os.getenv("PORT", 6969))
+	debug = os.getenv("DEBUG", "0") == "1"
+
+	threading.Thread(target=lambda: http.run(host="::", port=port, debug=debug, use_reloader=False)).start()
+
 	subprocess_manager_thread = threading.Thread(target=subprocess_manager)
 	subprocess_manager_thread.start()
